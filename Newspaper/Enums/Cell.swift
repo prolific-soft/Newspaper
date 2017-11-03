15d3f74f7fb9f5de7f54f24bf8a19ca3c2c4bf80
Chidi Emeh <chidiemeh184@gmail.com>
2017-11-03 12:28:49 -0400

    Tags TableViewController

--- Newspaper/Enums/Cell.swift
+++ Newspaper/Enums/Cell.swift
@@ -11,19 +11,26 @@ import Foundation
 
 enum Cell : String {
     
+    //Category Explore
     case searchTableViewCell = "searchTableViewCell"
     case categoryExploreTableViewCell = "categoryExploreTableViewCell"
     case categoryExploreCollectionViewCell = "categoryExploreCollectionViewCell"
+    //Explore Open
     case exploreOpenTableViewCell = "exploreOpenTableViewCell"
     case sourceOpenTableViewCell = "sourceOpenTableViewCell"
+    //Subscriptons
     case subscriptionSearchTableViewCell = "subscriptionSearchTableViewCell"
     case allArticlesTableViewCell = "allArticlesTableViewCell"
     case subsriptionSourcesTableViewCell = "subsriptionSourcesTableViewCell"
-    
+    //Star
     case starsSearchTableViewCell = "starsSearchTableViewCell"
     case starsTableViewCell = "starsTableViewCell"
-    
+    //Saved Pages
     case savedSearchTableViewCell = "savedSearchTableViewCell"
     case savedSourceTableViewCell = "savedSourceTableViewCell"
+    //Tags
+    case tagSearchTableViewCell = "tagSearchTableViewCell"
+    case allTagsTableViewCell = "allTagsTableViewCell"
+    case tagTableViewCell = "tagTableViewCell"
 
 }
---
+++ Newspaper/Tags/TagTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TagTableViewCell.swift
+//  Newspaper
+//
+//  Created by Chidi Emeh on 11/3/17.
+//  Copyright © 2017 Chidi Emeh. All rights reserved.
+//
+
+import UIKit
+
+class TagTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

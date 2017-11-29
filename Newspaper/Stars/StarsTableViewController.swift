682316692a5925e43776636f32cec6134dfcc67c
Chidi Emeh <chidiemeh184@gmail.com>
2017-11-28 23:22:55 -0500

    start implementation of deleting star articles

--- Newspaper/Stars/StarsTableViewController.swift
+++ Newspaper/Stars/StarsTableViewController.swift
@@ -25,8 +25,7 @@ class StarsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-         self.clearsSelectionOnViewWillAppear = true
+        self.clearsSelectionOnViewWillAppear = true
         
         self.tableView.estimatedRowHeight = self.tableView.rowHeight
         self.tableView.rowHeight = UITableViewAutomaticDimension
@@ -40,7 +39,6 @@ class StarsTableViewController: UITableViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         // Uncomment the following line to preserve selection between presentations
-        self.clearsSelectionOnViewWillAppear = true
         //self.loadData()
     }
 
@@ -126,55 +124,23 @@ class StarsTableViewController: UITableViewController {
 
     }//End loadData()
 
-    /*
-    // Override to support conditional editing of the table view.
+
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
 
 
-/// SEGUE
+/// MARK: - Navigation
 extension StarsTableViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
